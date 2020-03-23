2f928b25439e169d6f66c5f41529a41f7cd8ec13
sh2zqp <sh2zqp@gmail.com>
2020-03-23 15:25:49 +0800

    学习CRUD

--- src/main/java/com/sh2zqp/mybatis/MybatisTest.java
+++ src/main/java/com/sh2zqp/mybatis/MybatisTest.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
     public static void main(String[] args) throws Exception {
@@ -19,13 +20,38 @@ public class MybatisTest {
         // 获取sqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
-            // 操作CRUD，第一个参数：指定statement，规则：命名空间+“.”+statementId
-            // 第二个参数：指定传入sql的参数：这里是用户id
-            Category category = sqlSession.selectOne("com.sh2zqp.pojo.listCategory", 1);
-            System.out.println(category);
+            // add
+//            Category category1 = new Category();
+//            category1.setName("新增加的Category");
+//            sqlSession.insert("insertaddCategory", category1);
+
+            // delete
+//            Category c = new Category();
+//            c.setId(2);
+//            sqlSession.delete("deleteCategory", c);
+
+
+            // select
+//            Category c= sqlSession.selectOne("getCategory",3);
+
+            // update
+            Category c= sqlSession.selectOne("getCategory",3);
+            c.setName("修改了的Category名稱");
+            sqlSession.update("updateCategory",c);
+
+            listAll(sqlSession);
+
+            sqlSession.commit();
         } finally {
             sqlSession.close();
         }
 
     }
+    private static void listAll(SqlSession session) {
+        List<Category> cs = session.selectList("listCategory");
+        for (Category c : cs) {
+            System.out.println(c.getName());
+        }
+    }
+
 }
