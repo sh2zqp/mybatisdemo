73f69bcb0dc97bd1bb33b3923cf1a7432afe20c1
sh2zqp <sh2zqp@gmail.com>
2020-03-24 10:07:52 +0800

    多对多查询

--- src/main/java/com/sh2zqp/mybatis/MybatisTest.java
+++ src/main/java/com/sh2zqp/mybatis/MybatisTest.java
@@ -1,6 +1,8 @@
 package com.sh2zqp.mybatis;
 
 import com.sh2zqp.pojo.Category;
+import com.sh2zqp.pojo.Order;
+import com.sh2zqp.pojo.OrderItem;
 import com.sh2zqp.pojo.Product;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -70,10 +72,20 @@ public class MybatisTest {
 //            }
 
             // 多对一查询，产品对应分类
-            List<Product> ps = sqlSession.selectList("listProduct");
-            for (Product p : ps) {
-                System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
-            }
+//            List<Product> ps = sqlSession.selectList("listProduct");
+//            for (Product p : ps) {
+//                System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
+//            }
+
+            // 多对多操作，一张订单里 可以包含多种产品; 一种产品 可以出现在多张订单里
+            // 查询关系
+            listOrder(sqlSession);
+            // 建立关系
+            addOrderItem(sqlSession);
+            // 删除关系
+            deleteOrderItem(sqlSession);
+            // 修改关系
+            // 多对多不存在修改关系的做法，就是删除旧的，然后新增一条即达到修改的效果。
 
             sqlSession.commit();
         } finally {
@@ -86,6 +98,37 @@ public class MybatisTest {
         for (Category c : cs) {
             System.out.println(c.getName());
         }
+    }
+
+    private static void listOrder(SqlSession session) {
+        List<Order> os = session.selectList("listOrder");
+        for (Order o : os) {
+            System.out.println(o.getCode());
+            List<OrderItem> ois= o.getOrderItems();
+            for (OrderItem oi : ois) {
+                System.out.format("\t%s\t%f\t%d%n", oi.getProduct().getName(),oi.getProduct().getPrice(),oi.getNumber());
+            }
+        }
+    }
+
+    private static void addOrderItem(SqlSession session) {
+        Order o1 = session.selectOne("getOrder", 1);
+        Product p6 = session.selectOne("getProduct", 6);
+        OrderItem oi = new OrderItem();
+        oi.setProduct(p6);
+        oi.setOrder(o1);
+        oi.setNumber(200);
+
+        session.insert("addOrderItem", oi);
+    }
+
+    private static void deleteOrderItem(SqlSession session) {
+        Order o1 = session.selectOne("getOrder",1);
+        Product p6 = session.selectOne("getProduct",6);
+        OrderItem oi = new OrderItem();
+        oi.setProduct(p6);
+        oi.setOrder(o1);
+        session.delete("deleteOrderItem", oi);
     }
 
 }
---
+++ src/main/java/com/sh2zqp/pojo/OrderItem.java
@@ -0,0 +1,42 @@
+package com.sh2zqp.pojo;
+
+public class OrderItem {
+    private int id;
+    private int number;
+    private Order order;
+    private Product product;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public Order getOrder() {
+        return order;
+    }
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem.xml{" +
+                "id=" + id +
+                ", number=" + number +
+                ", order=" + order +
+                ", product=" + product +
+                '}';
+    }
+}
